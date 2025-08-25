80b1d39486c3e5c7a82b53c96e57e44ad3f0dbb1
asifmonda07 <asikbal7ai@gmail.com>
2025-08-25 22:47:36 +0530

    added

--- Q9/vite-project/src/App.jsx
+++ Q9/vite-project/src/App.jsx
@@ -2,13 +2,18 @@ import Header from './component/Header'
 
 import { Outlet } from 'react-router-dom'
 
+
 function App() {
   
   return (
     <>
       <Header/>
-      <main>
-        <Outlet/>
+      <main className="bg-blue-200 shadow-md sticky top-0 z-50">
+        <div className='max-w-6xl mx-auto px-4'>
+          <div className='flex justify-between items-center h-100'>
+            <Outlet/>
+          </div>
+        </div>
       </main>
     </>
   )
--- Q9/vite-project/src/component/Header.jsx
+++ Q9/vite-project/src/component/Header.jsx
@@ -1,60 +1,48 @@
 
-import {useNavigate,Link} from 'react-router-dom'
+import {useNavigate, NavLink} from 'react-router-dom'
+
+
 function Header() {
 
     const navigate=useNavigate()
 
-    const navIteam=[
+    const navItems=[
         {name:"Home", path:"/",},
         {name:"Aboout", path:"/about"},
         {name:"Contact", path:"/contact"}
     ]
 
-    const navbarStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    border:"1px solid black",
-    padding: "50px 28px",
-    fontFamily: "system-ui, sans-serif",
-    
-  };
-
-  const linksStyle = {
-    display: "flex",
-    gap: "24px",
-  };
 
 
 
   
   return (
-    <header >
-        <div>
-        <h1>Welcome Header</h1>
-            <nav style={navbarStyle}>
-                <div style={linksStyle}>
-                {
-                    navIteam
-                        .map((itm)=>(
-                            <Link
-                            key={itm.name}
-                            to={itm.path}
-                            onClick={(()=>{
-                                navigate(itm.path)
-                            })}
-                
-                            
-                            >
-                            {itm.name}
-                            </Link>
-                        ))
-                }
-                </div>
-            </nav>
-            
-            </div>
+     <header className="bg-purple-200 shadow-md sticky top-0 z-50">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          
+      
+          <h1 className="text-xl font-bold text-blue-600">Welcome Header</h1>
 
+         
+          <nav>
+            <ul className="flex space-x-6">
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <NavLink
+                    to={item.path}
+                    onClick={() => navigate(item.path)}
+                    className="text-black-600 rounded-3xl bg-blue-200 px-4 py-2
+                     hover:text-amber-50 font-medium transition-colors duration-200"
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
     
   )
